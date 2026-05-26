ec2a01a6d3762077954c182cbde3bd80922f9896
Theodore Havelka <tedhavelka66@gmail.com>
2026-05-26 01:28:27 +0000

    driver: introduce driver source file created for lecture 6 task 1.

---
+++ app/driver-module/drivers/nn-driver.c
@@ -0,0 +1,31 @@
+/**
+ * Iomico 2026 Zephyr Course sample driver source file
+ */
+
+#define DT_DRV_COMPAT nn_driver
+
+#include <zephyr/driver/sensor.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(nn_driver, LOG_LEVEL_INF);
+
+static int nn_driver_channel_get(const struct device *dev,
+                                 enum sensor_channel chan,
+                                 struct sensor_value *val)
+{
+	LOG_INF("Hello from nn_driver_channel_get()");
+	return 0;
+}
+
+static DEVICE_API(sensor, api_nn_driver) = {
+	.channel_get = nn_driver_channel_get;
+	.sample_fetch = NULL;
+};
+
+static int init(const struct device *dev)
+{
+	LOG_INF("nn_driver init() function called");
+	return 0;
+}
+
+DEVICE_DT_INST_DEFINE(0, init, NULL, NULL, NULL, POST_KERNEL,);
