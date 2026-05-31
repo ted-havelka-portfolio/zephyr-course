c4fd1a0172ada631498b66bb485aef06d42730a3
Theodore Havelka <tedhavelka66@gmail.com>
2026-05-31 01:20:23 +0000

    app: multiple changes to support refactoring of custom driver to make driver
    sources their own Zephyr module.  After much trail and error, project again
    compiles.

--- app/driver-module/include/nn-driver/nn-driver.h
+++
@@ -1,5 +0,0 @@
-/**
- * Iomico 2026 Zephyr Course sample driver header file
- */
-
-static int init(const struct device *dev);
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,4 +1,8 @@
-// #include <nn-driver.h>
+/*
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <../../../deps/modules/lib/nn-custom-driver/include/nn-custom-driver.h>
 
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
@@ -8,6 +12,8 @@
 #include <zephyr/logging/log.h>
 
 #define NN_DRIVER_NODE DT_NODELABEL(nn_driver0)
+
+#define DEV_USE_NN_DRIVER
 
 #if DT_NODE_HAS_STATUS(NN_DRIVER_NODE, okay)
 static const struct device *driver = DEVICE_DT_GET(DT_NODELABEL(nn_driver0));
@@ -22,6 +28,7 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+#ifdef DEV_USE_NN_DRIVER
 namespace {
 	void test(void) {
 		int32_t rc = 0;
@@ -48,20 +55,46 @@ namespace {
 		rc = sensor_sample_fetch(driver);
 	};
 };
+#endif // DEV_USE_NN_DRIVER
 
 int main(void)
 {
     bool led_state = true;
     static uint32_t call_count = 0;
+    uint32_t rc = 0;
 
     // Note Zephyr 3.7.0 (possibly a few earlier versions) require application
     // main() to return zero.  We do so here even on a device input/output
     // error:
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!gpio_is_ready_dt(&led)) goto end;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) goto end;
+
+#ifdef DEV_USE_NN_DRIVER
+    if (!device_is_ready(driver)) {
+        LOG_ERR("LED device not ready!  Exiting early . . .");
+        goto end;
+    }
 
     test();
+
+    // nn_driver_show_settings(driver);
+
+    // - DEV 0527 - debugging error when trying to reference driver struct:
+    // 1.
+    // const struct nn_device_data *data = (struct nn_device_data)driver->data;
+    // 2.
+    // #define DT_DRV_COMPAT nn_driver
+    // const struct nn_device_config *config = driver->config;
+    // 3.
+    // const struct nn_device_config *config = (struct nn_device_config)driver->config;
+    // if (config != NULL) {
+    //     config->show_settings(driver);
+    // }
+
+    rc = (uint32_t)show_settings(driver);
+    LOG_INF("- DEV 0528 - nn driver setting for active_led = %d", rc);
+#endif // DEV_USE_NN_DRIVER
 
     while (1) {
 	call_count++;
@@ -70,13 +103,17 @@ int main(void)
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
 
+#ifdef DEV_USE_NN_DRIVER
 	if ((call_count % 3) == 0) {
 		test_channel_get();
 	} else {
 		test_sample_fetch();
 	}
+#endif // DEV_USE_NN_DRIVER
 
         k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
+
+end:
     return 0;
 }
